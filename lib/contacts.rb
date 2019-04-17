e86ac7996dc577377a692b63b2d0b4a44c261f8c
Kish Siva <rodes88@msn.com>
2019-04-17 21:44:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -28,12 +28,12 @@ def remove_strawberry(contacts)
      }
    }
   
-  contacts.each do |person, data|
-    data.each do |data, info|
-      if info == "strawberry"
-        puts "defe"
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
-    end 
-  end 
+    end
+  end
   
 end
